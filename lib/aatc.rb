81f4c15a6a65cdd0e2046aafd5ff858adb940749
Resonious <metreckk@gmail.com>
2014-12-19 17:28:38 -0500

    Frameworking it up

--- lib/aatc.rb
+++ lib/aatc.rb
@@ -1,5 +1,53 @@
 require "aatc/version"
+require 'aatc/test'
 
 module Aatc
-  # Your code goes here...
+  class << self
+    attr_accessor :debug
+    def debug?; debug; end
+
+    SUBCOMMANDS = %i{
+      test
+    }
+
+    def run(argv)
+      if argv.empty?
+        fail 'Please specify a subcommand at least!'
+      end
+
+      argv.each_with_index do |arg, index|
+        if arg[0] == '-'
+          process_param(arg)
+        else
+          run_subcommand(arg, argv[index+1..-1])
+          break
+        end
+      end
+    end
+
+    def run_subcommand(subcommand, args)
+      case subcommand.to_sym
+      when *SUBCOMMANDS
+        require "aatc/#{subcommand}"
+        class_name    = subcommand
+        class_name[0] = subcommand[0].upcase
+
+        subcmd_class = const_get "Aatc::#{class_name}"
+        subcmd.new.run(args)
+
+      else
+        fail "There is no '#{subcommand}' subcommand!"
+      end
+    end
+
+    def process_param(param)
+      case param
+      when '-d', '--debug'
+        debug = true
+
+      else
+        fail "Invalid parameter #{param}"
+      end
+    end
+  end
 end
---
+++ lib/aatc/test.rb
@@ -0,0 +1,7 @@
+module Aatc
+  class Test
+    def run(args)
+      puts 'ayooooooooooooooooo'
+    end
+  end
+end
